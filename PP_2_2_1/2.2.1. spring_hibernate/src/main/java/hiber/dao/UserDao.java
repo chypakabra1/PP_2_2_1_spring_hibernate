545093e0d1842f14d18376881f5bd2a726521581
chypakabra1 <157878299+chypakabra1@users.noreply.github.com>
2024-05-27 18:09:15 +0300

    Add files via upload

---
+++ PP_2_2_1/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -0,0 +1,14 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+   void add(User user);
+
+   List<User> listUsers();
+
+   User getUserByCar(String model, int series);
+}
